da0109894acb6f2a5f2804be67f25f1af61acda0
Adrian Lamoru <adrian.lamoruferras@gmail.com>
2021-05-11 22:24:56 -0400

    Added create tag feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user(email='test@londonappdev.com', password='testpass'):
+
+    return get_user_model().objects.create_user(email, password)
+
 
 class MoodelTests(TestCase):
     def test_create_user_with_email_successful(self):
@@ -32,3 +39,12 @@ class MoodelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
